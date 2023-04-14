e2323273322749195393929f713a8a0e1b2ef90b
xanadu13 <o2xxing@gmail.com>
2023-04-14 21:31:21 +0900

    edit

--- woogi/script/script.js
+++ woogi/script/script.js
@@ -1,5 +1,9 @@
-$(function(){
-
-
-
+$(function() {
+  $('.profile-tabmenu li').click(function() {
+    let activeTab = $(this).attr('data-tab');
+    $('.profile-tabmenu li').removeClass('tab-on');
+    $('.tabcon').removeClass('t-on');
+    $(this).addClass('t-on');
+    $('#' + activeTab).addClass('tab-on');
+  })
 });
