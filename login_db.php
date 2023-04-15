815e1b86c636aae51d8f99fa24cc3ea58db6a96d
soyedpork27 <chan4728@kakao.com>
2023-04-15 23:50:31 +0900

    0415 hollys last push my boss

--- login_db.php
+++ login_db.php
@@ -21,11 +21,27 @@ $result01 = mysqli_query($con,$sql01);
 // 쿼리문 실행 결과를 객체로 저장
 $row01 = mysqli_fetch_assoc($result01);
 
+
+// 출력 확인 (완료)
 var_dump($row01);
 
 
+// 데이터베이스에서 가져온 정보 담기 시작
 
+//1. 멤버 코드
+$member_code = $row01['code'];
 
+//2. 유저 레벨 (1 : 일반, 2 : 탈퇴,  3 : , 4 : 관리자)
+$member_level = $row01['level'];
+
+//3. 이름
+$member_name = $row01['name'];
+
+//4. 이메일 
+$member_email = $row01['email'];
+
+// 5. 비밀번호
+$mamber_pw = $row['pw'];
 
 
 
