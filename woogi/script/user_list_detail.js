251a96f4e622436e025b0f0e7c2056166cefda70
xanadu13 <o2xxing@gmail.com>
2023-04-17 12:22:23 +0900

    script

--- woogi/script/user_list_detail.js
+++ woogi/script/user_list_detail.js
@@ -1,13 +1,19 @@
 $(function() {
 
-  alert('dd');
-
   $('.profile-tabmenu li').click(function() {
+    
     let activeTab = $(this).attr('data-tab');
-    $('.profile-tabmenu li').removeClass('tab-on');
+
+    // 탭메뉴 서식 바뀌는 스크립트
+    $('.profile-tabmenu li').find('a').removeClass('tab-on2');
+    $('.profile-tabmenu li').find('span').removeClass('tab-on3');
+    $(this).find('a').addClass('tab-on2');
+    $(this).find('span').addClass('tab-on3');
+
+    // 탭메뉴 내용 바뀌는 스크립트
     $('.tabcon').removeClass('t-on');
-    $(this).addClass('t-on');
-    $('#' + activeTab).addClass('tab-on');
+    $('#' + activeTab).addClass('t-on');
+
   });
 
 
