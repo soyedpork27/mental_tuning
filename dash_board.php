811a77aade30c31e2938a43ebaa797b903d18702
soyedpork27 <soyedpork27@gmail.io>
2023-04-19 16:16:43 +0900

    'ddadfasfwe'

--- dash_board.php
+++ dash_board.php
@@ -171,118 +171,96 @@
 
         </article>
         <div class="t_wrap sect-ctnt02">
-          <article class="ctnt_box02">
-            <h3 class="art-title mem-man-title">회원 관리</h3>
-
-
-
-
-            <div class="t_wrap table_wrap">
-
-              <div class="">
-                <table class="mem_table u_table">
-                  <caption class="u_table-title">신규 회원</caption>
-
-                  <thead class="u_table-head">
-                    <tr>
-                      <th>이름</th>
-                      <th>관심분야</th>
-                      <th>수강 강좌 수</th>
-                    </tr>
-                  </thead>
-
-                  <tbody class="u_table-body">
-                    <?php
-                    // 멤버 테이블에서 유저 레벨이 0,4 가 아닌 유저들을 뒤에서부터 4 개 불러온다
-                    $sql_member = "select * from member where level !=4 and level !=0 order by code desc limit 4";
-                    $result_member = mysqli_query($con, $sql_member);
-                    while($row_member = mysqli_fetch_assoc($result_member)){
-                    ?>
-                      <tr>
-                        <td><?=$row_member['name']?></td>
-                        <td><?=$row_member['interest']?></td>
-                        <td><?=$row_member['apply_class']?></td>
-                      </tr>
-                    <?php
-                    }
-                    ?>
-                  </tbody>
-
-                </table>
-
-              </div>
-              
-
-              <div>
-                <table class="tutor_table u_table">
-                  <caption class="u_table-title">신규 강사</caption>
-
-                  <thead class="u_table-head">
-                    <tr>
-                      <th>이름</th>
-                      <th>직업</th>
-                      <th>개설 강좌 수</th>
-                    </tr>
-                  </thead>
-
-                  <tbody class="u_table-body">
-                  <?php
-
-                    $sql_tutor = "select * from member where level=3 order by code desc limit 4 ";
-                    $result_tutor = mysqli_query($con, $sql_tutor);
-                    while($row_tutor = mysqli_fetch_assoc($result_tutor)){
-                    ?>
-                      <tr>
-                        <td><?=$row_tutor['name']?></td>
-                        <td><?=$row_tutor['job']?></td>
-                        <td><?=$row_tutor['open_class']?></td>
-                      </tr>
-                    <?php
-                    }
-                    ?>
-                  </tbody>
-                  
-                </table>
-
-              </div>
-
-            </div>
-
-
-          </article>
+          <div class="">
+            <article class="ctnt_box02 color002">
+              <h3 class="art-title mem-man-title">회원 관리</h3>
   
-          <article class="ctnt_box02">
-            <h3 class="art-title">신규 회원 가입 현황</h3>
-            
-          </article>
-
-          <article class="ctnt_box02 coulm">
-            <h3 class="hidden">세 번째 아티클 박스 제목</h3>
-            
-            <a href="#none" title="커뮤니티 관리" class="manage_bar t_wrap">
-              <span class="manage_title">
-                커뮤니티 관리
-              </span>
-              <i class="fa-solid fa-angle-right angle_60"></i>
-            </a>
-
-            <a href="#none" title="이벤트 관리" class="manage_bar t_wrap">
-              <span class="manage_title">
-                이벤트 관리
-              </span>
-              <i class="fa-solid fa-angle-right angle_60"></i>
-            </a>
-
-            <a href="#none" title="공지사항 관리" class="manage_bar t_wrap">
-              <span class="manage_title">
-                공지사항 관리
-              </span>
-              <i class="fa-solid fa-angle-right angle_60"></i>
-            </a>
-
-          </article>
-
-          <article class="ctnt_box02">
+  
+  
+  
+              <div class="t_wrap table_wrap">
+  
+                <div class="">
+                  <table class="mem_table u_table">
+                    <caption class="u_table-title">신규 회원</caption>
+  
+                    <thead class="u_table-head">
+                      <tr>
+                        <th>이름</th>
+                        <th>관심분야</th>
+                        <th>수강 강좌 수</th>
+                      </tr>
+                    </thead>
+  
+                    <tbody class="u_table-body">
+                      <?php
+                      // 멤버 테이블에서 유저 레벨이 0,4 가 아닌 유저들을 뒤에서부터 4 개 불러온다
+                      $sql_member = "select * from member where level !=4 and level !=0 order by code desc limit 4";
+                      $result_member = mysqli_query($con, $sql_member);
+                      while($row_member = mysqli_fetch_assoc($result_member)){
+                      ?>
+                        <tr>
+                          <td><?=$row_member['name']?></td>
+                          <td><?=$row_member['interest']?></td>
+                          <td><?=$row_member['apply_class']?></td>
+                        </tr>
+                      <?php
+                      }
+                      ?>
+                    </tbody>
+  
+                  </table>
+  
+                </div>
+                
+  
+                <div>
+                  <table class="tutor_table u_table">
+                    <caption class="u_table-title">신규 강사</caption>
+  
+                    <thead class="u_table-head">
+                      <tr>
+                        <th>이름</th>
+                        <th>직업</th>
+                        <th>개설 강좌 수</th>
+                      </tr>
+                    </thead>
+  
+                    <tbody class="u_table-body">
+                    <?php
+  
+                      $sql_tutor = "select * from member where level=3 order by code desc limit 4 ";
+                      $result_tutor = mysqli_query($con, $sql_tutor);
+                      while($row_tutor = mysqli_fetch_assoc($result_tutor)){
+                      ?>
+                        <tr>
+                          <td><?=$row_tutor['name']?></td>
+                          <td><?=$row_tutor['job']?></td>
+                          <td><?=$row_tutor['open_class']?></td>
+                        </tr>
+                      <?php
+                      }
+                      ?>
+                    </tbody>
+                    
+                  </table>
+  
+                </div>
+  
+              </div>
+  
+  
+            </article>
+    
+            <article class="ctnt_box02 color003">
+              <h3 class="art-title">신규 회원 가입 현황</h3>
+              
+            </article>
+  
+          </div>
+          
+          <!-- 캘린더 아티클 -->
+          <article class="ctnt_box03 color004">
             <h3 class="hidden">calander</h3>
             <div class="sec_cal">
             <div class="cal_nav">
@@ -305,11 +283,9 @@
                 </div>
               </div>
             </div>
-
-
+  
+  
           </article>
-
-
         </div>
 
       </section>
